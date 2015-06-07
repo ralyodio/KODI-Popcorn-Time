27c7470b05b39c4fb54cb39a1c8321816e22661b
Henrik Ankersø <privat@diblo.dk>
2015-06-07 22:43:31 +0200

    Fixing structure

--- plugin.video.kodipopcorntime/addon.py
+++ plugin.video.kodipopcorntime/addon.py
@@ -1,6 +1,6 @@
 import sys, os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'site-packages'))
-from kodipopcorntime import monkey_patches, plugin
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'lib'))
+from kodipopcorntime.common import plugin, PLATFORM, RESOURCES_PATH
 
 if __name__ == '__main__':
     try:
@@ -9,4 +9,55 @@ if __name__ == '__main__':
         import xbmc
         import traceback
         map(xbmc.log, traceback.format_exc().split("\n"))
-        raise
+        sys.exit(0)
+
+if PLATFORM["os"] not in ["android", "linux", "windows", "darwin"]:
+    plugin.notify(plugin.addon.getLocalizedString(30302) % PLATFORM, delay=15000)
+    sys.exit(0)
+
+from kodipopcorntime.caching import cached_route
+from kodipopcorntime.utils import ensure_fanart
+from kodipopcorntime.providers import yify
+from kodipopcorntime.player import TorrentPlayer
+
+# Cache TTLs
+DEFAULT_TTL = 24 * 3600 # 24 hours
+
+@plugin.route("/")
+@ensure_fanart
+def index():
+    for provider in providers['movie']:
+        yield {
+            "label": provider['label'],
+            "icon": path.join(RESOURCES_PATH, 'media', provider['icon']),
+            "thumbnail": path.join(RESOURCES_PATH, 'media', provider['thumbnail']),
+            "path": plugin.url_for(provider['path'])
+        }
+    return yify.list()
+
+@plugin.route("/list/<provider>/<item>/<page>")
+@ensure_fanart
+def list():
+    return yify.list(item, page)
+
+@plugin.route("/browse/<provider>/<item>/<page>")
+#contents = ['files', 'songs', 'artists', 'albums', 'movies', 'tvshows', 'episodes', 'musicvideos']
+@cached_route(ttl=DEFAULT_TTL, content_type="movies")
+@ensure_fanart
+def browse(item, page):
+    return yify.browse(item, page)
+
+@plugin.route("/search/<provider>")
+def search(provider):
+    query = plugin.keyboard("", plugin.addon.getLocalizedString(30001))
+    if query:
+        Plugin.redirect(Plugin.url_for("search_query", provider=provider, query=query, page=1))
+
+@plugin.route("/search/<provider>/<query>/<page>")
+@ensure_fanart
+def search_query(query, page):
+    return yify.search_query(query, page)
+
+@plugin.route("/play/<uri>")
+def play(uri):
+    TorrentPlayer().init(uri, sub).loop()
